a9a0108c977d2476bd88114d7186d662d5891847
Sameem <sameem_97@outlook.com>
2024-02-28 18:05:26 +0000

    Define networking module

--- aks-terraform/networking-module/variables.tf
+++ aks-terraform/networking-module/variables.tf
@@ -13,5 +13,23 @@ variable "location" {
 variable "vnet_address_space" {
     description = "Address space for the Virtual Network (VNet)"
     type = list(string)
-    default = ["10.0.0.0/16"]
+    sensitive = true
+}
+
+variable "cp_subnet_space" {
+    description = "Address space for control plane nodes"
+    type = list(string)
+    sensitive = true
+}
+
+variable "wn_subnet_space" {
+    description = "Address space for worker nodes"
+    type = list(string)
+    sensitive = true
+}
+
+variable "source_address_prefix" {
+    description = "Source prefix for network security rule"
+    type = string
+    sensitive = true
 }
